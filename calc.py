647ce96b85867e265793318ce7dca744574c83e6
Yaroslav Cherednyk <azazely85@gmail.com>
2017-03-27 12:43:15 +0300

    calc (+,-,/,%,**,//)

---
+++ calc.py
@@ -0,0 +1,24 @@
+#calc
+a = float(input())
+b = float(input())
+c = input()
+if (c=="+"):
+    print(a+b)
+elif(c=="-"):
+    print(a - b)
+elif (c == "/" and b!=0):
+    print(a / b)
+elif (c == "/" and b == 0):
+    print("Деление на 0!")
+elif (c == "*"):
+    print(a * b)
+elif (c == "mod" and b != 0):
+    print(a % b)
+elif (c == "mod" and b == 0):
+    print("Деление на 0!")
+elif (c == "pow"):
+    print(a ** b)
+elif (c == "div" and b != 0):
+    print(a // b)
+elif (c == "div" and b == 0):
+    print("Деление на 0!")
